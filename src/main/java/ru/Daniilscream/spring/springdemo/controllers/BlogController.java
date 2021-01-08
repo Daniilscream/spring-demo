afb300b762e823f9d0498d0c4f36e03bab710bd2
Daniilscream <danilvisn@list.ru>
2021-01-08 12:36:06 +0300

    first commit

---
+++ src/main/java/ru/Daniilscream/spring/springdemo/controllers/BlogController.java
@@ -0,0 +1,14 @@
+package ru.Daniilscream.spring.springdemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+
+    @GetMapping("/blog")
+    public String blogMain(Model model){
+        return "blog-main";
+    }
+}
